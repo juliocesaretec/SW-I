283ba36a8551328d4fc00e70d4749bfc1e594c78
juliocesaretec <161508817+juliocesaretec@users.noreply.github.com>
2025-02-14 16:55:24 -0300

    aula 02

---
+++ exemplo01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi sou o Julio</p>
+    <?php
+        echo "<p>Oi sou o Julio</p>";
+        $nome = "julio";
+        echo "<p>Oi sou o $nome </p>";
+        echo "<p>Oi sou o". $nome."</p>";
+
+    ?>
+
+</body>
+</html>
---
+++ exemplo02.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 4;
+    $b = "Dalmatas 101";
+
+    $s = $a + $b;
+
+    echo $s;
+
+
+?>
